be7b39e57d9f03fca0bb476da40a56bf0442a56c
sasikorn-dream <sasikorn.k@github.com>
2020-03-28 14:55:58 +0700

    changed

--- app/src/main/java/th/ac/kku/cis/mobileapp/beautycafe/MainActivity.kt
+++ app/src/main/java/th/ac/kku/cis/mobileapp/beautycafe/MainActivity.kt
@@ -1,12 +1,27 @@
 package th.ac.kku.cis.mobileapp.beautycafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var mAuth : FirebaseAuth? = null
+    private val TAG : String = "Main Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        if (mAuth!!.currentUser != null){
+            startActivity(Intent(this@MainActivity,ResultActivity::class.java))
+            finish()
+        }
+        main_email_Btn.setOnClickListener {
+            startActivity(Intent(this@MainActivity,LoginActivity::class.java))
+            finish()
+        }
     }
 }
